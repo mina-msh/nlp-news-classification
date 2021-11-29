2576f981cf471a3b7c7f59828edecb4db14eb13d
peyman-dashtestani <91415895+peyman-dashtestani@users.noreply.github.com>
2021-11-29 23:54:02 +0330

    ploting data

--- core.py
+++ core.py
@@ -1,8 +1,21 @@
-import nltk as nt
-import pandas as ps
+from __future__ import unicode_literals
+import nltk as nk
+import pandas as pd
 import numpy as np
 import autocorrect as ac
-import hazm 
 from autocorrect import spell
+from __future__ import unicode_literals
+from hazm import *
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-dataset = "NLP datatset title.xlsx"
+dataset = "NLP datatset title.csv"
+
+df = pd.read_csv(dataset , encoding='utf-8' )
+print(df.head())
+
+sns.countplot(x=df["news path"])
+plt.show()
+# normalizer = Normalizer()
+# normalizer.normalize('اصلاح نويسه ها و استفاده از نیم‌فاصله پردازش را آسان مي كند')
