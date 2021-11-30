d304a30e83a7d014cbf3531c6334e3f3bac66726
peyman-dashtestani <91415895+peyman-dashtestani@users.noreply.github.com>
2021-11-30 15:14:02 +0330

    import data - adding lables - segmenting data

--- core.py
+++ core.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 import nltk as nk
-import pandas as pd
 import numpy as np
 import autocorrect as ac
 from autocorrect import spell
@@ -12,10 +11,24 @@ import matplotlib.pyplot as plt
 
 dataset = "NLP datatset title.csv"
 
+# reading and ploting data
 df = pd.read_csv(dataset , encoding='utf-8' )
 print(df.head())
-
-sns.countplot(x=df["news path"])
+sns.countplot(x=df["News path"])
 plt.show()
-# normalizer = Normalizer()
-# normalizer.normalize('اصلاح نويسه ها و استفاده از نیم‌فاصله پردازش را آسان مي كند')
+
+# x is input data
+# y is output data
+X = df["Title"]
+Y = df["News path"]
+
+# adding int index for labels from 0 to 3
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+print(Y)
+sns.countplot(Y)
+plt.show()
+# segmentation to 80% test and 20% train
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
